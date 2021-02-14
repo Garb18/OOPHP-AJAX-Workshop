4b16275fa50a637379f0c5cba57bd9f14c2b094b
Garb18 <46030758+Garb18@users.noreply.github.com>
2021-02-14 09:38:05 +0000

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,10 @@
 <?php
 	session_start();
 	
-	$host = '172.16.11.22:3306';
-	$dbname = 'garb1_18_todo';
-	$user = 'garb1_todo';
-	$pass = '9Iekg64@';
+	$host = ip;
+	$dbname = name;
+	$user = user;
+	$pass = password;
 
 	try {
 		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
